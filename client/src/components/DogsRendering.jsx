45ab72c83e8218f5200321ae49555375463a1bdf
Maximo Dip Aparicio <maxcodingg@gmail.com>
2023-11-05 18:38:17 -0300

    idk

--- client/src/components/DogsRendering.jsx
+++ client/src/components/DogsRendering.jsx
@@ -1,28 +1,24 @@
-import { useState, useEffect } from "react"
-import { getDogsRequest } from "../api/dogs.api.js"
-import { DogCard } from "../components/DogCard.jsx"
+import { useState, useEffect } from 'react'
+import { getDogsRequest } from '../api/dogs.api.js'
+import { DogCard } from '../components/DogCard.jsx'
 
-export default function DogsRendering () {
-    const [dogs, setDogs] = useState([])
+export default function DogsRendering() {
+  const [dogs, setDogs] = useState([])
 
-    useEffect(() => {
-        const loadDogs = async () => {
-            const response = await getDogsRequest()
-            setDogs(response.data)
-            console.log(response.data)
-            
-        }
-        loadDogs()
-    }, [])
-    return (
-        <div>
-            <h1>Dogs</h1>
-            {
-                dogs.map(dog => (
-                    <DogCard dog={dog} key={dog.id}/>
-                ))
-            }
-        </div>
-    )
+  useEffect(() => {
+    const loadDogs = async () => {
+      const response = await getDogsRequest()
+      setDogs(response.data)
+      console.log(response.data)
+    }
+    loadDogs()
+  }, [])
+  return (
+    <div>
+      <h1>Dogs</h1>
+      {dogs.map((dog) => (
+        <DogCard dog={dog} key={dog.id} />
+      ))}
+    </div>
+  )
 }
-
