c06df14cf82be57feac6287b5972d6852f4d7753
Dani <danicabral15@outlook.com>
2023-11-05 21:07:37 -0300

    asdsad

--- client/src/context/DogContext.jsx
+++ client/src/context/DogContext.jsx
@@ -1,7 +1,7 @@
 import { createContext } from "react";
 
 const DogContext = createContext();
-
+console.log("jaja");
 
 const DogContextProvider = ({ children }) => {
     return <DogContext.Provider>{children}</DogContext.Provider>
