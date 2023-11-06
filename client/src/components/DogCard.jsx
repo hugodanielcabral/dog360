a7cb48b268aceea92f000729d7c222e5229875cc
Dani <danicabral15@outlook.com>
2023-11-05 21:34:18 -0300

    Agregado estilos a DogCard

--- client/src/components/DogCard.jsx
+++ client/src/components/DogCard.jsx
@@ -1,13 +1,16 @@
-
 export const DogCard = ({ dog }) => {
   return (
-    <>
-        <h1 className="text-2xl">Raza: {dog.nombre}</h1>
-        <img src={dog.imagen} alt="" />
-        <p>{dog.descripcion}</p>
-        <p>{dog.tamanio}</p>
-        <p>{dog.esperanza_de_vida}</p>
-        <p>{dog.personalidad}</p>     
-     </>
-  )
-}
+    <div className="bg-white rounded-lg shadow-lg p-4 m-4 max-w-md">
+      <h1 className="text-2xl font-bold mb-2 text-gray-800">Raza: {dog.nombre}</h1>
+      <img
+        src={dog.imagen}
+        alt=""
+        className="rounded-md w-full h-auto object-cover"
+      />
+      <p className="text-gray-700 text-sm mt-2">{dog.descripcion}</p>
+      <p className="text-gray-700 text-sm">Tamaño: {dog.tamanio}</p>
+      <p className="text-gray-700 text-sm">Esperanza de vida: {dog.esperanza_de_vida} años</p>
+      <p className="text-gray-700 text-sm">Personalidad: {dog.personalidad}</p>
+    </div>
+  );
+};
